0cfd078c6517dd5748cf751718865081dddb1449
Mike Sharp <msharp23@hotmail.co.uk>
2023-05-23 22:57:38 +0100

    chore: improved startup messaging, also you can just copy paste the address the listener gives you into the client instead of needing to do an ip lookup manually.

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
 import { Server } from "socket.io";
+var ip = require('ip');
 const fs = require("fs");
 const player = require("sound-play");
 const path = require("path");
-
+import chalk  from 'chalk';
 const io = new Server(3000, {
   cors: {
     origin: "*",
   },
 });
 console.log("listening on port 3000");
+console.log(chalk.red(`Before you run the 'soundboard-client' project, please add the environment variable below to your .env file`));
+console.log(chalk.green(`VITE_SOUND_SERVER_ADDRESS=http://${ip.address()}:3000`))
+console.log(chalk.yellow('^^^This is the the address the client will use on your local network to talk to this app to listen for soundboard sound requests'))
 io.on("connection", (socket) => {
   const soundManifest = JSON.parse(
     fs.readFileSync(path.join(__dirname, `../media/sound-manifest.json`))
