c8b396c8f8590b22693043b65f46b595b556bc0d
Mike Sharp <msharp23@hotmail.co.uk>
2023-07-23 00:27:45 +0100

    feat: added one-command build tooling using workspaces

--- packages/soundboard-listener/src/build-manifest.ts
+++ packages/soundboard-listener/src/build-manifest.ts
@@ -5,13 +5,9 @@ console.log("building manifest...");
 
 const soundEntries: { name: any; label: any; color: string }[] = [];
 
+const directory = process.cwd();
 
-// pkg gets your working directory wrong if you use process.cwd() when the app is compiled, so let's test for the bad directory name and use
-// an alternative if we get a positive result.
-const directory = __dirname.indexOf('/snapshot') > -1 ? process.execPath : __dirname;
-
-
-fs.readdirSync(path.join(directory, `../media/`)).forEach((file: any) => {
+fs.readdirSync(path.join(directory, `/media/`)).forEach((file: any) => {
   if (file.indexOf(".mp3") > -1) {
     soundEntries.push({
       name: file.split(".")[0],
@@ -22,7 +18,7 @@ fs.readdirSync(path.join(directory, `../media/`)).forEach((file: any) => {
 });
 console.log(JSON.stringify({ sounds: soundEntries }));
 fs.writeFileSync(
-  path.join(directory, `../media/sound-manifest.json`),
+  path.join(directory, `/media/sound-manifest.json`),
   JSON.stringify({ sounds: soundEntries })
 );
 console.log(
--- packages/soundboard-listener/src/main.ts
+++ packages/soundboard-listener/src/main.ts
@@ -1,3 +1,4 @@
+declare var process: { pkg: { [key: string]: string }; cwd: any };
 const express = require("express");
 const app = express();
 const serverPort: number = 3000;
@@ -9,13 +10,21 @@ const fs = require("fs");
 const path = require("path");
 const sound = require("sound-play");
 
-// pkg gets your working directory wrong if you use process.cwd() when the app is compiled, so let's test for the bad directory name and use
-// an alternative if we get a positive result.
-const directory = __dirname.indexOf('/snapshot') > -1 ? process.execPath : __dirname;
+let clientDirectory: string = "";
+
+const directory = process.cwd();
+
+if (process.pkg) {
+  // runtime is compiled with pkg
+  clientDirectory = process.cwd();
+} else {
+  // runtime is not compiled with pkg
+  clientDirectory = "../soundboard-client/dist";
+}
 
 const io = new Server(serverPort, {
   cors: {
-    origin: '*',
+    origin: "*",
     methods: ["GET", "POST", "OPTIONS"],
   },
 });
@@ -24,32 +33,29 @@ console.log(`soundboard listener is listening on port ${serverPort}`);
 
 io.on("connection", (socket) => {
   const soundManifest = JSON.parse(
-    fs.readFileSync(path.join(directory, `../media/sound-manifest.json`))
+    fs.readFileSync(path.join(directory, `/media/sound-manifest.json`))
   );
   socket.emit("get-sounds", soundManifest.sounds);
   socket.on("play-sound", async (requestedSound) => {
     const soundObject = JSON.parse(requestedSound);
-    const filePath = path.join(directory, `../media/${soundObject.name}.mp3`);
+    const filePath = path.join(directory, `/media/${soundObject.name}.mp3`);
     sound.play(filePath);
   });
 });
 
-if (
-  fs.existsSync(path.join(directory, "../../soundboard-client/dist/index.html"))
-) {
+if (fs.existsSync(path.join(clientDirectory, `/index.html`))) {
   console.log(
     `Soundboard client build exists, attempting to host on port ${clientPort}...`
   );
   fs.writeFileSync(
-
-    path.join(directory, "../../soundboard-client/dist/server-address.js"),
+    path.join(clientDirectory, `/server-address.js`),
     `
         var serverAddress = 'ws://${ip.address()}:${serverPort}';
     `
   );
 
   app.use(
-    express.static(path.join(directory, "../../soundboard-client/dist"), {
+    express.static(path.join(clientDirectory, `/`), {
       port: clientPort,
     })
   );
